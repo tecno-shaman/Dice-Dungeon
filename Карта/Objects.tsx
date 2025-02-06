c7569703165a6de4f25041859b44ebf7347afb32
susmaster12 <y4roslaw07@yandex.com>
2025-02-07 01:00:48 +0300

    Add files via upload

---
+++ Карта/Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Objects" tilewidth="16" tileheight="16" tilecount="320" columns="10">
+ <image source="Objects.png" width="160" height="512"/>
+</tileset>
